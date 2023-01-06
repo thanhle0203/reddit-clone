77e640776dac03fd09629e73cbd2d2ce96af3292
Thanh Le <thanhlevan0203@gmail.com>
2023-01-06 15:55:11 -0800

    Create VerificationToken model

--- src/main/java/com/example/redditclone/model/VerificationToken.java
+++ src/main/java/com/example/redditclone/model/VerificationToken.java
@@ -1,4 +1,23 @@
 package com.example.redditclone.model;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "token")
 public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String token;
+
+    @OneToOne(fetch = LAZY)
+    private User user;
+    private Instant expiryDate;
 }
