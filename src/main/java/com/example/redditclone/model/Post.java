20238d9570bb86a6c88b0be6a57e1eb79c449565
Thanh Le <thanhlevan0203@gmail.com>
2023-01-06 15:33:12 -0800

    Create post model

--- src/main/java/com/example/redditclone/model/Post.java
+++ src/main/java/com/example/redditclone/model/Post.java
@@ -1,4 +1,46 @@
 package com.example.redditclone.model;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor
+
+import java.time.Instant;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long postId;
+
+    @NotBlank(message = "Post Name cannot be empty or NULl")
+    private String postName;
+
+    @Nullable
+    private String url;
+
+    @Nullable
+    @Lob
+    private String description;
+    private Integer voteCount;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+    private Instant createDate;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
+
 }
