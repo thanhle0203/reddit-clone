b97d6f2c73466482a2aabd04d0ba9f75c9a38a56
Thanh Le <thanhlevan0203@gmail.com>
2023-01-06 16:03:35 -0800

    Create Vote model

--- src/main/java/com/example/redditclone/model/Post.java
+++ src/main/java/com/example/redditclone/model/Post.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
--- src/main/java/com/example/redditclone/model/Vote.java
+++ src/main/java/com/example/redditclone/model/Vote.java
@@ -1,4 +1,27 @@
 package com.example.redditclone.model;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@NoArgsConstructor
+@Entity
+@Builder
 public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteId;
+    private VoteType voteType;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private  User user;
+
+
 }
